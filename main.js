727f5dce4f4f064d295731c122f0527aee2add35
lihsin <lihsin0427@gmail.com>
2021-12-10 11:17:47 +0800

    串接 API 獲取資料

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-document.addEventListener('DOMContentLoaded', () => {
-  // Start !
+document.addEventListener('DOMContentLoaded', (e) => {
+  fetch('https://top-250-movies.herokuapp.com/api/v1/movies/top')
+  .then((response)=> {
+    return response.json();
+  })
+
+  .then(( results ) => {
+      console.log(results)
+    });
+  // .then(({ results }) => {
+  //   results.forEach((u)=>{
+  //     console.log(u.name)
+  //   });
+  // });
 })
