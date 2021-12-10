17d3f4f70e254fb7b4f2d01ed65331f690b804a8
lihsin <lihsin0427@gmail.com>
2021-12-10 11:46:58 +0800

    任務二 ： 根據格式渲染資料

--- main.js
+++ main.js
@@ -5,11 +5,34 @@ document.addEventListener('DOMContentLoaded', (e) => {
   })
 
   .then(( results ) => {
-      console.log(results)
+      // console.log(results)
+      results.forEach((u)=>{
+        const tbody = document.querySelector('tbody')
+        tbody.innerHTML +=`<tr>
+        <td>${ u.rank }</td>
+        <td><a href="${ u.link }">${ u.title }</a></td>
+        <td>${ u.year }</td>
+        <td>${ u.director }</td>
+        <td>${ u.main_stars }</td>
+        <td>${ u.rating }</td>
+        </tr> `
+      })
     });
-  // .then(({ results }) => {
-  //   results.forEach((u)=>{
-  //     console.log(u.name)
-  //   });
-  // });
+
+
+
+
+
+    const pagination = document.querySelector('.pagination-container')
+    pagination.innerHTML = `
+      <span class="back-page">＜</span>
+      <div class="pagination">
+        <span>1</span>
+        <span>2</span>
+        <span>3</span>
+      </div>
+      <span class="next-page">＞</span>
+      <div class="pagination-counter">
+      </div> 
+      `
 })
